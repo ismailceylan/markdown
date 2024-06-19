2dda24d48073aa70713491354132f6e0c4330940
Ismail Ceylan <ismaillceylan@gmail.com>
2024-06-20 00:10:54 +0300

    build: move demo and make ready for publishing

---
+++ demo.js
@@ -0,0 +1,80 @@
+import Markdown from "./src/markdown";
+import { EOS } from "./src/constants";
+
+const md = new Markdown(
+{
+	plugins:
+	{
+		bold:
+		[
+			{
+				mode: "match",
+				with: "**"
+			},
+			{
+				mode: "eat",
+				until: "**",
+				name: "content"
+			}
+		],
+		link:
+		[
+			{
+				mode: "match",
+				with: "["
+			},
+			{
+				mode: "eat",
+				until: "]",
+				name: "link-content"
+			},
+			{
+				mode: "match",
+				with: "("
+			},
+			{
+				mode: "eat",
+				until: ")",
+				name: "paranthesis-content",
+				useValue:
+				{
+					"link-and-title":
+					[
+						{
+							mode: "eat",
+							until: " ",
+							name: "link"
+						},
+						{
+							mode: "match",
+							with: " "
+						},
+						{
+							mode: "eat",
+							until: EOS,
+							name: "title"
+						}
+					],
+
+					"only-link":
+					[
+						{
+							mode: "eat",
+							until: EOS,
+							name: "link",
+						}
+					]
+				}
+			}
+		]
+	}
+});
+
+console.time("render");
+
+const rendered = md.render(
+`Hello **World**! [How](sasd) are **you** babe?` );
+
+console.timeEnd("render");
+
+console.log(md);
